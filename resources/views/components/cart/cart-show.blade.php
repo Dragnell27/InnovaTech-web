1755fb6e7808ceb9c3962353a3ec6346fc719bcd
Alzate1 <115501748+Alzate1@users.noreply.github.com>
2023-08-01 02:50:02 -0500

    cart

--- resources/views/components/cart/cart-show.blade.php
+++ resources/views/components/cart/cart-show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.contenedor')
 @section('title','Home')
 @section('component')
+
 <head>
     <link rel="stylesheet" href="{{ asset('css/carrito.css') }}">
 </head>
@@ -12,152 +13,121 @@
         @if (Cart::getContent()->count() <= 0) <section>
             <div id="carritovacio">
                 <h1>Su carrito está vacio</h1>
-               <img src="{{ asset('img/carro-vacio.png') }}" alt="">
+                <img src="{{ asset('img/carro-vacio.png') }}" alt="">
             </div>
-        @else
-        <div id="CarritoCompraTitulo">
-            <h3> <i class="bi bi-cart4"></i>
-                <span class="text-danger">Carrito de compras</span>
-            </h3>
+            @else
+            <div id="CarritoCompraTitulo">
+                <h3> <i class="bi bi-cart4"></i>
+                    <span class="text-danger">Carrito de compras</span>
+                </h3>
 
-        </div>
-        <div class="row g-5 ml-20">
-            @php
-               $datos = Cart::getContent();
-         
-               @endphp
-            <div class="col-md-5 col-lg-4 order-md-last">
-                <h4 class="d-flex justify-content-between align-items-center mb-3">
-                    <span class="text-danger">Resumen de compra</span>
-                
-                    <span class="badge bg-danger rounded-pill">{{ $datos->count() }}</span>
-
-                </h4>
-                <ul class="list-group mb-3">
-                    @foreach ($datos as $items )
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <div>
-                            <h6 class="my-0">{{ $items->name }}</h6>
-
-                            <div class="small-product">
-                                <img src=" {{ $items->attributes["image"] }}" >
-
-                            </div>
-                            <small class="text-body-secondary">{{ $items->attributes["desc"]}}</small>
-                        </div>
-                    </li>
-                    @endforeach
-                    <li class="list-group-item d-flex justify-content-between bg-body-tertiary">
-                        <div class="text-success">
-                            <h6 class="my-0">Total (COP)</h6>
-                        </div>
-                        @if ($items->attributes["discount"]==0)
-                        <span class="text-success" id="resultado">−${{ $items->quantity * $items->price }}</span>
-                        @else
-                        @php
-                          $descuento=($items->price * $items->attributes["discount"]) /100
-                        @endphp
-                        <span class="text-success" id="resultado">${{ ($items->price * $items->quantity)-$descuento}}</span>
-                        @endif
-                    </li>
-                </ul>
-                <form class="card p-2">
-                    <div class="input-group">
-                        <a name="" id="" class=" w-100 btn btn-primary  btn-lg" href="{{ route('LuEnvio') }}"
-                            role="button">Continuar compra
-                        </a>
-                    </div>
-                </form>
             </div>
+            <div class="row g-5 ml-20">
 
-            <div class=" col-lg-6  rounded ">
-                 @foreach (Cart::getContent() as $items)
-                <ul class="list-group mb-3 ">
-                    <li class="list-group-item  justify-content-between lh-sm" style="margin-left: 10px;">
-                        <table class="">
-                            <tbody>
-                              
-                                    <tr>
-                                        <td>
+                <div class=" col-lg-6  rounded order-md-first ">
+                    @foreach (Cart::getContent() as $items)
+                    <div class="container w-100 mb-2">
+                        <div class="card w-100">
+                            <div class="card w-100">
+                                <ul class="list-inline  mt-2 mb-2 w-100">
 
-                    <li class="d-flex">
-                        <div class="small-product">
-                            <img src=" {{ $items->attributes["image"] }}" >
+                                    <li class="list-inline-item "style="margin-right: 15px"> <img
+                                            src=" {{ $items->attributes["image"] }}" ">
+                                    </li>
 
-                        </div>
-                    </li>
-                    <li class="d-flex mt-4">
-                        <small>
-                            <button> envio rapido</button>
-                        </small>
-                    </li>
+                                    <li class="list-inline-item col-4" >
+                                        <h4>
+                                            {{ $items->name }}
+                                        </h4>
 
-                    </td>
-                    <td>
-                        <span>
-                            <p>
-                                {{ $items->attributes["desc"] }}
-                            </p>
-                        </span>
-                    </td>
-                    <td>
-                        <li class=" d-flex ">
+                                        <p>
+                                            {{ $items->attributes["desc"] }}
+                                        </p>
+                                    </li>
 
-                            <span>
-                                <p>
-                                    @if ($items->attributes["discount"])
-
-                                    <strong id="precio">{{ $items->price }}</strong>
+                                    @if ($items->attributes["discount"]==0)
+                                    <li class="list-inline-item mr-4" style="text-align: right" ><strong id="precio">
+                                            {{ $items->price }}
+                                        </strong>
+                                    </li>
                                     @else
-
-                                    <strong id="precio">{{ $items->price }}</strong>
-                                    <strong id="precio">{{ $items->attributes["discount"] }}</strong>
-
+                                    @php
+                                    $descuento=($items->price * $items->attributes["discount"]) /100
+                                    @endphp
+                                    <li class="list-inline-item">
+                                        <strong id="precio">
+                                            {{ $items->price }}
+                                        </strong>
+                                        <span class=""
+                                            id="precio">${{ ($items->price * $items->quantity)-$descuento}}</span>
+                                    </li>
                                     @endif
 
+                                    <li class="list-inline-item ">
+                                        <button class="buttonsMM" id="aumentar">
+                                            <i class="bi bi-plus-circle-fill"></i>
+                                        </button>
+                                        <span class="badge bg-danger rounded-pill" id="cantidad">1</span>
 
-                                </p>
-                            </span>
+                                        <button class="buttonsMM ml-4" id="disminuir"><i
+                                                class="bi bi-dash-circle-fill"></i></button>
+                                                <li style="margin-left: 70%">
+                                                    <a href="" class="ml-2">eliminar</a>
+                                                    <a href="" class="ml-2">Lista de deseos</a>
+                                                </li>
+                                            </li>
+                                   
+                                    <a name id class=" w-40 btn btn-primary ml-4" style="margin-top: -16px" href="{{ route('LuEnvio') }}"
+                                        role="button">Continuar compra
+                                    </a>
+                                </ul>
+                            </div>
+                        </div>
 
+                        @endforeach
+                    </div>
+
+                </div>
+
+                <div class="col-md-5 col-lg-4 order-md-last">
+                    <h4 class="d-flex justify-content-between align-items-center mb-3">
+                        <span class="text-danger">Resumen de compra</span>
+
+                    </h4>
+                    <ul class="list-group mb-3">
+                        <li class="list-group-item d-flex justify-content-between bg-body-tertiary">
+                            <div class="">
+                                <h4 class="my-0">Productos
+                                    <span class="badge bg-danger rounded-pill"> {{ $items->count() }}</span>
+                                </h4>
+                            </div>
                         </li>
-                        <li class="d-flex mt-3">
-                            <button class="buttonsMM">
-                                <spam>Lista de Deseos</spam>
-                            </button>
+                        <li class="list-group-item d-flex justify-content-between bg-body-tertiary">
+                            <div class="text-success">
+                                <h6 class="my-0">Total (COP)</h6>
+                            </div>
+                            @if ($items->attributes["discount"]==0)
+                            <span class="text-success" id="resultado">${{ $items->quantity * $items->price }}</span>
+                            @else
+                            @php
+                            $descuento=($items->price * $items->attributes["discount"]) /100
+                            @endphp
+                            <span class="text-success"
+                                id="resultado">${{ ($items->price * $items->quantity)-$descuento}}</span>
+                            @endif
                         </li>
-
-                    </td>
-                    <td class="mt-2">
-                        <li class="d-flex mt-4">
-                            <button class="buttonsMM" id="aumentar">
-                                <i class="bi bi-plus-circle-fill"></i>
-                            </button>
-
-                            &nbsp;&nbsp;
-                            <span class="badge bg-danger rounded-pill mr-2" id="cantidad">{{ $items->quantity }}</span>
-                            &nbsp;&nbsp;
-                            <button class="buttonsMM"id="disminuir"><i class="bi bi-dash-circle-fill"></i></button>
-                        </li>
-                        <li class="d-flex mt-4">
-                            <button class="buttonsMM" >
-                                <span>Eliminar</sp>
-                            </button>
-                        </li>
-                    </td>
-                    </tr>
-
-
-                    </tbody>
-                    </table>
-                    </li>
-                </ul>
-                 @endforeach
-
+                    </ul>
+                    <form class="card p-2">
+                        <div class="input-group">
+                            <a name="" id="" class=" w-100 btn btn-primary  btn-lg" href="{{ route('LuEnvio') }}"
+                                role="button">Continuar compra
+                            </a>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
 
-
-        @endif
+            @endif
 
     </main>
     <footer>
