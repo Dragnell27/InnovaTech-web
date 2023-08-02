1091f726a2d6845dda40faba657d822acdf90bf5
Alzate1 <115501748+Alzate1@users.noreply.github.com>
2023-08-02 16:33:36 -0500

    Producto terminado

--- public/js/producto.js
+++ public/js/producto.js
@@ -2,42 +2,25 @@ function myFunction(smallImg){
     var fullImg=document.getElementById("imgBox")
 fullImg.src=smallImg.src;
 }
-
-
-// {{ env(API='/products') }}
-// fetch (API)
-// .then(response =>{
-//     if(!response.ok){
-// throw new Error('No se pudo obtener los datos del api')
-//     }
-//     return response.json();
-// })
-// .then(data=>{
-//     'name', 
-//     'code', 
-//     'desc', 
-//     'discount',
-//     'price',
-//     'images',
-//     'category' ,
-//     'colors',
-//     'mark' ,
-//     'tags'
-// })
-
-$(document).ready(function(){
-    $.ajax({
-        url:"{{ env('API').'/products' }}",
-        method:'GET',
-        dataType:'json',
-        success:function(data){
-            console.long('Response:',data);
-        },
-        error:function(error){
-            console.error('error al obtener los datos del api',error);
-        },
-        success:function(response){
-            $('#id').val(response.data[0].id)
-        }
-    });
-});
+//Camilo Alzate Aqui se ha Consumido el API de Productos
+window.addEventListener('load',async()=>{
+var productID=1;
+try {
+    const response =await fetch("http://localhost/proyecto_web/public/api/products/"+productID);
+    const data = await response.json();
+    const producto=data.data[0];
+    console.log(producto);
+    document.getElementById("name").textContent = producto.name;
+    document.getElementById("desc").textContent= producto.desc;
+    document.getElementById("price").textContent= '$'+producto.price;
+    document.getElementById("color").textContent= producto.colors;
+    document.getElementById("imgBox").src= producto.images
+    document.getElementById("imageOne").src= producto.images;
+    document.getElementById("imageTwo").src= producto.images;
+    document.getElementById("imageThree").src= producto.images;
+    document.getElementById("imagefour").src= producto.images;
+ 
+} catch (error) {
+    console.error('Error al obtener los datos del producto')
+}
+})
--- resources/views/products/singleProduct.blade.php
+++ resources/views/products/singleProduct.blade.php
@@ -4,25 +4,21 @@
 
 <head>
     <link rel="stylesheet" href="{{ asset('css/productos.css') }}">
-
 </head>
 <section class="bg-light">
     <div class="container pb-5">
         <div class="row">
+
             <div class="col-lg-5 mt-5">
                 <div class="imgContainer ">
-                    <img id="imgBox" src="{{ asset('img/airpods1.jpg') }}" alt="Card image cap">
+                    <img id="imgBox"alt="Card image cap">
 
                 </div>
                 <div class="small-product">
-                    <img class="" src="{{ asset('img/V-Producto-3.jpg') }}" alt="Producto Imagen 2"
-                        onclick="myFunction(this)">
-                    <img class=" " src="{{ asset('img/V-Producto-4.jpg') }}" alt="Producto Imagen 3"
-                        onclick="myFunction(this)">
-                    <img class=" " src="{{ asset('img/V-Producto-2.jpg') }}" alt="Producto Imagen 1"
-                        onclick="myFunction(this)">
-                    <img class=" " src="{{ asset('img/airpods1.jpg') }}" alt="Producto Imagen 1"
-                        onclick="myFunction(this)">
+                    <img id="imageOne" src="" alt="Producto Imagen 2" onclick="myFunction(this)">
+                    <img id="imageTwo" src="" alt="Producto Imagen 3" onclick="myFunction(this)">
+                    <img id="imageThree" src="" alt="Producto Imagen 1" onclick="myFunction(this)">
+                    <img id="imagefour" src="" alt="Producto Imagen 1" onclick="myFunction(this)">
 
                 </div>
             </div>
@@ -30,39 +26,25 @@
             <div class="col-lg-7 mt-5">
                 <div class="card">
                     <div class="card-body">
-                        <div class="text-center">
-                            <h1>PRODUCTO</h1>
+                            <div class="text-center">
+                                <h1 id="name"> </h1>
+                            </div>
+                            <div class="text-right">
+                                <h1 class="my-badge bg-danger" id="price"></h1>
 
                         </div>
-                        <div class="text-right">
-                            <h1 class="my-badge bg-danger"id="price">$25.000</h1>
+                            <h6>Descripcion:</h6>
+                            <p id="desc"></p>
 
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <h6>Color :</h6>
+                                </li>
+                                <li class="list-inline-item">
+                                    <h6 class="text-muted"><strong id="color"></strong></h6>
+                                </li>
+                            </ul>
                         </div>
-
-                        <ul class="list-inline">
-                            <li class="list-inline-item">
-                                <h6>Categoria:</h6>
-                            </li>
-                            <li class="list-inline-item">
-                                <h6 class="text-muted" id="categoria"><strong>Estuches</strong></h6>
-                            </li>
-                        </ul>
-
-                        <h6>Descripcion:</h6>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp incididunt ut
-                            labore et dolore magna aliqua. Quis ipsum suspendisse. Donec condimentum elementum
-                            convallis. Nunc sed orci a diam ultrices aliquet interdum quis nulla.</p>
-
-                        <ul class="list-inline">
-                            <li class="list-inline-item">
-                                <h6>Color :</h6>
-                            </li>
-                            <li class="list-inline-item">
-                                <h6 class="text-muted" id="color"><strong>Negro</strong></h6>
-                            </li>
-                        </ul>
-
-                    
                         <div class="row pb-3">
                             <div class="col d-grid mb-2">
                                 <button type="submit" class="w-100 btn btn-danger btn-lg" name="submit"
@@ -77,11 +59,10 @@
             </div>
             <!-- comentarios -->
             <div id="contenedorcomentarios" class="container-fluid">
-                <div class="col-lg-12 mt-5">
-                    <h3>Comentarios</h3>
+                <h3 class="text-center">Comentarios</h3>
+                <div class="col-lg-12">
                     <!-- Aquí se agregarán los comentarios -->
                     <div id="comentarios"></div>
-
                     <!-- Formulario de comentario -->
                     <form id="comment-form">
                         <div class="form-group">
@@ -101,7 +82,7 @@
 
     <div class="container-fluid d-flex justify-content-center align-items-center" id="contenedortitulo">
         <div class="div ">
-            <button class="div btn-danger w-100 ">Ver Comentarios Del Producto</button>
+            <button class="div btn-danger w-100" >Ver Comentarios Del Producto</button>
         </div>
     </div>
     <script src="{{ asset("js/producto.js") }}">
