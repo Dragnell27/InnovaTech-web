ff1912208fa175588fdb5dd18324c515a6d00550
Alzate1 <115501748+Alzate1@users.noreply.github.com>
2023-08-01 20:56:30 -0500

    Frontend productos proceso

--- public/js/producto.js
+++ public/js/producto.js
@@ -1,5 +1,43 @@
 function myFunction(smallImg){
     var fullImg=document.getElementById("imgBox")
 fullImg.src=smallImg.src;
-
 }
+
+
+// {{ env(API='/products') }}
+// fetch (API)
+// .then(response =>{
+//     if(!response.ok){
+// throw new Error('No se pudo obtener los datos del api')
+//     }
+//     return response.json();
+// })
+// .then(data=>{
+//     'name', 
+//     'code', 
+//     'desc', 
+//     'discount',
+//     'price',
+//     'images',
+//     'category' ,
+//     'colors',
+//     'mark' ,
+//     'tags'
+// })
+
+$(document).ready(function(){
+    $.ajax({
+        url:"{{ env('API').'/products' }}",
+        method:'GET',
+        dataType:'json',
+        success:function(data){
+            console.long('Response:',data);
+        },
+        error:function(error){
+            console.error('error al obtener los datos del api',error);
+        },
+        success:function(response){
+            $('#id').val(response.data[0].id)
+        }
+    });
+});
