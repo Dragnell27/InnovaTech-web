f5454734fe15ed7fc6b636a61845d40acaf9675f
Nakizzu <105331480+Nakizzu@users.noreply.github.com>
2023-08-08 00:08:53 -0500

    Footer-100%

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,5 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 <footer>
     <div id="divfooter">
         <div class="modovertical1" id="infofooter">
@@ -15,9 +14,7 @@
               <a class="btn text-white btn-floating" href="https://api.whatsapp.com/send?phone=+573117622089" role="button" target="_blank"><i class="bi bi-whatsapp"></i></a>
               <a class="btn text-white btn-floating"  href="https://instagram.com/innovatechcol?igshid=MzRlODBiNWFlZA==" role="button" target="_blank"><i class="bi bi-instagram"></i></a>
         </section>
-
             </div>
-
         </div>
         <div class="modovertical2" id="mover-mapa">
             <b>
@@ -25,13 +22,12 @@
                     <h4 style="color: white;">Nuestra Ubicación</h4>
                 </center>
             </b>
-
             <iframe id="map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3979.9257994164057!2d-76.52583988464063!3d3.4511708976337547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a8b0a333d68d%3A0xebda6e9fe3f0e6d4!2sCl.%2052%20%233-80%2C%20Cali%2C%20Valle%20del%20Cauca!5e0!3m2!1sen!2sco!4v1650902596097!5m2!1sen!2sco&t=satellite&layer=m"
                 allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
-    <div id="divcopyright" class="text-center" class="modovertical">
+    <div id="divcopyright" class="text-center p-1" class="modovertical">
         <p style="color: white;"><b>© 2023 - TODOS LOS DERECHOS RESERVADOS</b></p>
     </div>
 </footer>
