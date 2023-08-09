a7181872877cbd0bbbfaaabf9a8120763aaea95c
Nakizzu <105331480+Nakizzu@users.noreply.github.com>
2023-08-09 11:19:45 -0500

    Update Preloader

--- public/js/preloader.js
+++ public/js/preloader.js
@@ -1,13 +1,8 @@
 /* JavaScript Preloader Juliana */
-//  Esta funcion sirve para ocultar el preloader después de un pequeño retraso si hay internet
-// Si no hay internet, el preloader seguirá mostrándose hasta que se recupere la conexión
-
 window.addEventListener("load", function () {
     const preloader = document.querySelector('.centrado');
-
-    // Verificar si hay internet (online)
     if (navigator.onLine) {
-        // Si hay internet, ocultar el preloader después de 0.5 segundos (500 milisegundos)
+        // Si hay internet se oculta
         setTimeout(function() {
             preloader.style.display = 'none';
         }, 500);
@@ -16,13 +11,11 @@ window.addEventListener("load", function () {
         // El preloader seguirá mostrándose hasta que se recupere la conexión
     }
 });
-
-// Evento para detectar cambios en la conexión a internet (online/offline)
+// conexión a internet (online/offline)
 window.addEventListener('offline', function() {
     const preloader = document.querySelector('.centrado');
     preloader.style.display = 'flex';
 });
-
 window.addEventListener('online', function() {
     const preloader = document.querySelector('.centrado');
     setTimeout(function() {
