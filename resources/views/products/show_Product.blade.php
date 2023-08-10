c0d9c879354e7b3beefa72a8b5401da65b488f87
Nakizzu <105331480+Nakizzu@users.noreply.github.com>
2023-08-09 20:54:30 -0500

    Low-Update

--- resources/views/products/show_Product.blade.php
+++ resources/views/products/show_Product.blade.php
@@ -5,7 +5,10 @@
 
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/productos.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/comments.css') }}">
+
 
 </head>
 <section class="bg-light">
@@ -88,15 +91,32 @@
     </div>
     </div>
 
-    <div class="container-fluid d-flex justify-content-center align-items-center" id="contenedortitulo">
-        <div class="div ">
-            <button class="div btn-danger w-100" id="CargarBoton" onclick="BotonCargar">Ver Comentarios Del Producto</button>
+    <div class="container">
+      <div class="row" id="secciondecomentarios">
+        <div class="col-md-8 offset-md-2">
+          <div class="container-fluid" style="margin: 20px 0px;">
+            <div class="red-div">
+              <h2 style="color: white;">Comentarios De Los Usuarios</h2>
+            </div>
+          </div>
+          <div class="container-fluid d-flex flex-row justify-content-center align-items-center">
+            <div style="margin-bottom: 30px;">
+              <button type="button" class="btn btn-danger text-white">Más Reciente</button>
+              <button type="button" class="btn btn-danger text-white">Mayor Calificación</button>
+              <button type="button" class="btn btn-danger text-white">Menor Calificación</button>
+            </div>
+          </div>
+          <div class="comments" style="align-content: left">
+            <div class="comment">
+              <p class="fechacomentario">21/05/2023</p>
+              <h4>Rodrigo Arango</h4>
+              <p><i style= "color: yellow;">★★★★★</i></p>
+              <p>¡Me encanta este producto! Es de alta calidad y vale cada centavo.</p>
+            </div>
+          </div>
         </div>
-    </div>
+        </div>
 <script src="{{ asset('js/producto.js')}}"></script>
 <script src="{{ asset('js/comentarios.js')}}"></script>
-
-
-
 </section>
 @endsection
