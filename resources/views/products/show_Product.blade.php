2b7aa5307867eb21e7ca86c68635ab2bddd32e01
Nakizzu <105331480+Nakizzu@users.noreply.github.com>
2023-08-12 12:04:51 -0500

    Update Comment

--- resources/views/products/show_Product.blade.php
+++ resources/views/products/show_Product.blade.php
@@ -6,7 +6,6 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/productos.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/comments.css') }}">
 
 
     </head>
@@ -128,4 +127,5 @@
     </div>
     </section>
     <script src="{{ asset('js/producto.js') }}"></script>
+    <script src="{{ asset('js/comments.js') }}"></script>
 @endsection
