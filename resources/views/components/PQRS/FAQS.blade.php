606072df4d3220b92b3fbbae82bb2c5ff372e303
Nakizzu <105331480+Nakizzu@users.noreply.github.com>
2023-08-18 13:41:29 -0500

    Upgrade Home

--- resources/views/components/PQRS/FAQS.blade.php
+++ resources/views/components/PQRS/FAQS.blade.php
@@ -21,34 +21,37 @@
                     required>
                 <select name="type" id="param_type" class=" form-select" required>
                     <option value="">Selecciona el tipo</option>
-                  
+
                 </select>
                <textarea id="" style="resize: none" cols="20" rows="2" name="body" required class="forms form-control" placeholder="Tu mensaje">
 
                </textarea>
                <div class="center text-center"><input type="checkbox" name="" id="" required>
                 <span class="subMessages">Estoy de acuerdo con la politica de datos</span></div>
-                
+
                 <div class="center text-center">
                     <input type="submit" id="submitBtn" class="btn btn-success text-center center">
                 </div>
             </div>
         </form>
         </div>
-        
+
     </div>
 </div>
 <div id="chat-container" class="actionBt">
     <div id="chat-circle">
-        <div id="round-image">
-            <img src="{{ asset('img/ventana-de-chat.png') }}" alt="" width="50px" style="padding: 5px">
+
+<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-headset" viewBox="0 0 16 16" style="align-items: center">
+  <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z"/>
+</svg>
+            {{-- <img src="{{ asset('img/ventana-de-chat.png') }}" alt="" width="50px" style="padding: 5px"> --}}
         </div>
-        
-    </div>
+
+
 </div>
- 
+
  <script src="{{ asset('js/faqs.js') }}"></script>
- 
+
 <script>
 
 </script>
