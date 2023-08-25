42f82f3e845db77b91780282963da96598bc0854
JSvalencia18 <jusevaga2003@gmail.com>
2023-08-24 22:19:27 -0500

    about
    cambios

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -21,26 +21,26 @@
                 <!-- El resto de las secciones se mantienen igual, pero se agregan IDs para los enlaces de la barra de navegación -->
                 <section id="section-mision" class="section-about" >
                     <h2 class="text-dark">Nuestra Misión</h2>
-                    <img src="{{ asset('img/mision.jpg') }}" alt="Imagen de la Misión" class="img-fluid mb-3">
-                    <p></p>
+                    <img src="{{ asset('img/mision.jpg') }}" alt="Imagen de la Misión" class="img-fluid mb-5">
+                    <h3 class="text-dark"><p>La plataforma de servicios online INNOVA-TECH, es la encargada de incrementar la productividad, efectividad y comunicación de clientes del sector de  la tecnología en Cali, Yumbo, Dosquebradas y Pasto con la micro-empresa innova tech occidente así aumentando sus ventas y servicios online  y dando oportunidad a que esta empresa pueda expandirse mucho mas.</p></h3>
                 </section>
 
                 <section id="section-historia" class="section-about">
                     <h2 class="text-dark">Nuestra Historia</h2>
                     <img src="{{ asset('img/mision_empresa.jpg') }}" alt="Imagen de la Historia" class="img-fluid mb-3">
-                    <p></p>
+                    <h3 class="text-dark"><p></p></h3>
                 </section>
 
                 <section id="section-equipo" class="section-about">
                     <h2 class="text-dark">Nuestro Equipo</h2>
                     <img src="{{ asset('img/mision_empresa.jpg') }}" alt="Imagen de Nuestro Equipo" class="img-fluid mb-3">
-                    <p></p>
+                    <h3 class="text-dark"><p></p></h3>
                 </section>
 
                 <section id="section-vision" class="section-about">
                     <h2 class="text-dark">Nuestra Visión</h2>
                     <img src="{{ asset('img/mision_empresa.jpg') }}" alt="Imagen de Nuestra Visión" class="img-fluid mb-3">
-                    <p></p>
+                    <h3 class="text-dark"><p>En 2028 la plataforma de servicios y comercio  INNOVA-TECH logrará posicionarse y permanecer como una de las principales plataformas digitales que conecte los clientes con la empresa misma a nivel nacional. Priorizando así al máximo la atención al cliente al momento de obtener productos e información de manera eficaz. Brindando así más practicidad, rapidez y seguridad al momento de automatizar los procesos necesarios en compras de productos. </p></h3>
                 </section>
             </main>
         </div>
