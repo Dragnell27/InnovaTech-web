31b292febf021ffc766800f7be26dba37261e735
Jhon Murillo <jhonalexandermurillo19@gmail.com>
2023-08-26 11:21:02 -0500

    Arreglo de detalles para compras

--- public/js/sales.js
+++ public/js/sales.js
@@ -11,17 +11,33 @@ try {
                     for (const sales of venta) {
                         console.log(sales);
                         var fecha = (sales.fecha).split('T')[0];
+                        switch (sales.estado) {
+                            case "Recibido":
+                                var colorElement = "#1CE838";
+                                break;
+                            case "Entregado":
+                                var colorElement = "#3364FF";
+                                break;
+                            case "Pendiente":
+                            var colorElement = "#E87807";
+                                 break;
+                            case "Cancelado":
+                            var colorElement = "#FF0505";
+                                break;
+                            default:
+                                break;
+                        }
                         var images = sales.imagen.split(':');
                         datos += `
                         <div class=" border-bottom col-12 row mb-2">
-                        <div class="col-md-3">
+                        <div class="col-md-3 p-3">
                         <img src="https://innovatechcol.com.co/img/productos/${images[0]}" alt="Producto" class="img-fluid">
                         </div>
 
-                        <div class="col-md-9 card-body>
-                        <h3 class="mt-4">${sales.producto}</h3>
-                        <p class="mt-3">Estado:${sales.estado} </p>
-                        <p class="mt-2">Fecha de compra: ${fecha}</p>
+                        <div class="col-md-9 card-body p-3">
+                        <h3 class="mt-4 producto">${sales.producto}</h3>
+                        <font color="${colorElement}"><p class="mt-3 estado-texto">${sales.estado} </p></font>
+                        <p class="mt-2 compra">Fecha de compra: ${fecha}</p>
 
                     <div class="col-12 text-end p-3">
                       <a href="show_product "> <button class="btn btn-primary">Ver producto</button></a>
--- resources/views/sales/shopping.blade.php
+++ resources/views/sales/shopping.blade.php
@@ -3,10 +3,25 @@
 <link rel="stylesheet" href="{{ asset('css/mis_compras.css') }}">
 <div class="container">
     <div class="row justify-content-center " id="content">
-
     </div>
 </div>
+<style>
+    .estado-texto {
+    font-weight: bold;
+    font-size: 18px;
+}
+    .producto{
+    font-weight: bold;
+}
 
+    .estado-texto.entregado{
+    color: green;
+}
+
+    .Pendiente {
+    color: yellow;
+}
+</style>
 <script>
     var id = {{ Auth::user()->id }};
 </script>
