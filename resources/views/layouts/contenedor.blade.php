23bf538f4b87db1a3f89ef0e8414ca125edfc3c8
Darkj <joanhurtado134@outlook.es>
2023-08-28 11:26:23 -0500

    icons aligments

--- resources/views/layouts/contenedor.blade.php
+++ resources/views/layouts/contenedor.blade.php
@@ -32,10 +32,11 @@
     <header>
         <nav class="nav">
             <div class="class">
+                         {{--  links in the sidebar  --}}
                 <a href="{{ url('/') }}">
-                    <img src="{{ asset('img/logo-i.png') }}" id="imagen-logo">
+                    <img src="{{ asset('img/logo-i.png') }}" id="imagen-logo" style="margin-bottom: 11px;">
                 </a>
-                <a id="btn-menu" onclick="toggleSidebar()"><img src="{{ asset('img/Menu.png') }}" id="menu-logo">
+                <a id="btn-menu" onclick="toggleSidebar()"><img src="{{ asset('img/Menu.png') }}" style="margin-bottom: 11px;" id="menu-logo">
          
                 </a>
                 <div class="sidebar" style="background-color: white;" id="sidebar">
@@ -47,7 +48,7 @@
                         @auth
                             <li class="links">
                                 <a class="ocultar" href="{{ route('users.show', Auth::user()->id) }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                                 </svg>
@@ -59,7 +60,7 @@
                         @endauth ()
                         <li class="links justify-content-center align-items-center">
                             <a class="ocultar" href="{{ route('wishlist.index') }}"  style="display: flex;  align-items: center;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bag-heart-fill" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-bag-heart-fill" viewBox="0 0 16 16">
                                     <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
                                 </svg>
                                  <span class="spanText">Mi lista de deseos
@@ -71,8 +72,8 @@
                             <li  class="links">
                                 <form action="{{ Route('cart.show') }}" method="get">
                                     <div id="icono" class=" ">
-                                        <button type="submit" id="btnCarrito" class="" style="margin-top: 0 !important; height: 40px !important; width: 60px !important">
-                                            <img src="{{ asset('img/carro_compra.png') }}" style="margin-bottom: 6px" width="30px" height="30px"
+                                        <button type="submit" id="btnCarrito" class="" style="margin-top: 0 !important; height: 27px !important; width: 40px !important; margin-left:3px" >
+                                            <img src="{{ asset('img/carro_compra.png') }}"  width="25px" height="25px"
                                                 alt="">
                                             <span class="" id="counter"> 
                                                 @if (Auth::check())
@@ -89,7 +90,7 @@
                                         </button>
                                         <span class="text-center  " >
                                             <a id="cartLink" style="position: absolute;
-                                            top: 14px;
+                                            top: 6px;
                                             margin-left: 3px;
                                             " href="" class="links">Carrito</a></span>
                                        
@@ -100,7 +101,7 @@
                             
                      
                         @auth
-                            <li class="links"><a href="{{ route('logout') }}"> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                            <li class="links"><a href="{{ route('logout') }}"> <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
                                 <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                               </svg>
@@ -113,34 +114,45 @@
                     </div>
                 </div>
             </div>
+
+
+
+            {{--  Links activos, en el home  --}}
             <ul class="nav-links">
                 <i class="uil uil-times navCloseBtn"></i>
                 @auth
-                    <li class="links"><a href="{{ route('users.show', Auth::user()->id) }}"> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <li class="links"><a href="{{ route('users.show', Auth::user()->id) }}"> <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                 </svg>
-                <span>Mi cuenta</span></a></li>
+                <span class="spansText">Mi cuenta</span></a></li>
                 @else
-                    <li class="links"><a href="{{ route('login') }}"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z"/>
-                        <path fill-rule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
-                      </svg>
-                      <span>Iniciar sesión</span>
-                      </a></li>
+                    <li class="links">
+                        <a href="{{ route('login') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z"/>
+                                <path fill-rule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
+                            </svg>
+
+                            <span class="spansText">Iniciar sesión</span>
+                      </a>
+                      </li>
                 @endauth ()
-                <li  class="links"> <a href="{{ route('wishlist.index') }}">  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bag-heart-fill" viewBox="0 0 16 16">
-                    <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
-                </svg>
-             <span>Lista de deseos</span>
+                <li  class="links"> 
+                    <a href="{{ route('wishlist.index') }}">  
+                        <svg xmlns="http://www.w3.org/2000/svg"  width="27" height="27" fill="currentColor" class="bi bi-bag-heart-fill" viewBox="0 0 16 16">
+                        <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
+                    </svg>
+                    <span class="spansText">Lista de deseos</span>
                  </a></li>
-                
+                {{--  carrito del home  --}}
                  <li  class="links">
                     <form action="{{ Route('cart.show') }}" method="get">
                         <div id="icono" class=" ">
-                            <button type="submit" id="btnCarrito" class="" style="margin-top: 0 !important; height: 25px !important; width: 60px !important;margin-bottom: 1px">
-                                <img src="{{ asset('img/carro_compra.png') }}" style="margin-bottom: 6px" width="30px" height="30px"
-                                    alt="">
+                            <button type="submit" id="btnCarrito" class="" style="margin-top: 0 !important; height: 30px !important; width: 60px !important; margin-bottom: 7px;">
+                                <img src="{{ asset('img/carro_compra.png') }}"  style="margin-top: 5px; color: black;" width="27px" height="27px"
+                                    alt="">  
+                                   
                                 <span class="" id="counter"> 
                                     @if (Auth::check())
                                         <?php $CartCount = Cart::session(Auth::user()->id)
@@ -162,16 +174,16 @@
                 </li>
                 @auth
                     <li class="links"><a href="{{ route('logout') }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
                             <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                           </svg>
-                        <span>Cerrar sesion</span></a></li>
+                        <span class="spansText">Cerrar sesion</span></a></li>
                 @endauth ()
             </ul>
 
             <i class="uil search-icon " id="searchIcon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" style="margin-bottom: 11px;" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                 
               </svg></i>
